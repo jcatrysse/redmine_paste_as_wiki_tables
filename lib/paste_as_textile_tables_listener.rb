100593772c59396e1281d5b8d2b8ec2b7f1d7ecc
knt419 <knt419@gmail.com>
2018-04-25 07:46:41 +0900

    add .editorconfig

--- lib/paste_as_textile_tables_listener.rb
+++ lib/paste_as_textile_tables_listener.rb
@@ -1,3 +1,3 @@
 class PasteAsTextileTablesListener < Redmine::Hook::ViewListener
-  render_on :view_issues_form_details_bottom, partial: "paste_as_textile_tables/script"
+  render_on :view_issues_form_details_bottom, partial: 'paste_as_textile_tables/script'
 end
