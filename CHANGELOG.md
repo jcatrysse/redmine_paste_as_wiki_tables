7e4a16141f1bfd206df0a4d42285b0389385b673
Jan Catrysse <jan.catrysse@trustteam.be>
2023-08-23 14:28:22 +0200

    Add preview on note edit

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
 # CHANGELOG
+## 0.1.1
+* Add preview for pasted clipboard images
+
 ## 0.1.0
 * Optimize code structure (hooks)
 * Refactoring of the javascript
--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_paste_as_wiki_tables do
   name 'Redmine Paste As Wiki Tables plugin'
   author 'Jan Catrysse'
   description 'This plugin enhances the functionalities of Redmine by handling paste events for tables and clipboard images.'
-  version '0.1.0'
+  version '0.1.1'
   url 'https://github.com/jcatrysse/redmine_paste_as_wiki_tables'
   author_url 'https://github.com/jcatrysse/'
 
